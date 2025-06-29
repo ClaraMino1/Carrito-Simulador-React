ec7a238c9766c0c13c105e0bfa3b6fb2e7fbc74d
Clara Miño <claraa.mino12@gmail.com>
2025-06-29 20:36:12 -0300

    filtrado terminado

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,20 +1,28 @@
 import { Input, List } from 'antd';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './SearchBar.css';
 
 const { Search } = Input;
 
 const SearcherBar = ({books}) => {
   const navigate = useNavigate();
 
-const handleSearch = () => {
-  if (filteredBooks.length > 0) {
-    navigate(`/BookDetails/${filteredBooks[0].id}`);
-  }
-};
-    //query = lo que el usuario escribe
+  const handleSearch = () => { //se llama al apretar enter o presionar el boton buscar
+    if (filteredBooks.length > 0) { //si hay resultados filtrados(solo toma el primer resultado) entonces redirecciona a la página de detalles con el id del libro filtrado
+      navigate(`/BookDetails/${filteredBooks[0].id}`);
+    }
+  };
+
+  const handleSelect = (bookId) => {//se llama cuando se hace click en un item de la lista de items que coinciden con la busqueda
+    navigate(`/BookDetails/${bookId}`);
+    setQuery("");// Limpia el input
+  };
+
+
+  //query = lo que el usuario escribe
   const [query, setQuery] = useState('');//inicialmente no hay busqueda 
-  
+    
   const filteredBooks = books.filter((book) => //devuelve un array con los libros filtrados 
     book.title.toLowerCase().includes(query.toLowerCase())
     //por cada libro agarra el titulo y busca si la query que se ingresa en la searchbar coincide
@@ -31,15 +39,23 @@ const handleSearch = () => {
         style={{ marginBottom: 20 }}
       />
 
-    {query !== "" ? (// si se escribió algo en la searchbar devuelve los titulos que coinciden
+      {query !== "" ? (// si se escribió algo en la searchbar devuelve los titulos que coinciden
         <List
-            bordered
-            dataSource={filteredBooks} //libros que coinciden
-            renderItem={(item) => <List.Item>{item.title}</List.Item>}
+          bordered
+          dataSource={filteredBooks} //libros que coinciden
+          // renderItem={(item) => <List.Item>{item.title}</List.Item>}
+          renderItem={(item) => {
+            return(
+              <List.Item
+                className='item-filter'
+                onClick={() => handleSelect(item.id)}>
+                  {item.title}
+              </List.Item>
+            )}}
         />
-    ) : null}
-    </div>
-  );
+      ) : null}
+      </div>
+    );
 };
 
 export default SearcherBar;
