5bf7d0fd06c8aeb5f9289ffef1c9a3348232ce92
Clara Miño <claraa.mino12@gmail.com>
2025-06-29 13:46:01 -0300

    ahora la searchbar solo devuelve los titulos en caso de que coincidan con la busqueda

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -4,28 +4,32 @@ import React, { useState } from 'react';
 const { Search } = Input;
 
 const SearcherBar = ({books}) => {
+    //query = lo que el usuario escribe
   const [query, setQuery] = useState('');//inicialmente no hay busqueda 
   
-  const filteredBooks = books.filter((book) => //devuelve un true o false
+  const filteredBooks = books.filter((book) => //devuelve un array con los libros filtrados 
     book.title.toLowerCase().includes(query.toLowerCase())
+    //por cada libro agarra el titulo y busca si la query que se ingresa en la searchbar coincide
   );
 
-  return (
+  return(
     <div style={{ maxWidth: 600, margin: 'auto', padding: 20 }}>
       <Search
         placeholder="Buscar libro"
         enterButton="Buscar"
         size="large"
-        onSearch={(value) => setQuery(value)}
-        onChange={(e) => setQuery(e.target.value)}
+        // onSearch={(value) => setQuery(value)} =>se ejecuta cuando el usuario presiona Enter o el botón
+        onChange={(e) => setQuery(e.target.value)} //se ejecuta en cada tecla presionada => actualiza query (genera el efecto de busqueda en tiempo real).
         style={{ marginBottom: 20 }}
       />
 
-      <List
-        bordered
-        dataSource={filteredBooks}
-        renderItem={(item) => <List.Item>{item.title}</List.Item>}
-      />
+    {query !== "" ? (// si se escribió algo en la searchbar devuelve los titulos que coinciden
+        <List
+            bordered
+            dataSource={filteredBooks} //libros que coinciden
+            renderItem={(item) => <List.Item>{item.title}</List.Item>}
+        />
+    ) : null}
     </div>
   );
 };
