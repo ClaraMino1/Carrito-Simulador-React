044d7e3e51c1dd690cd72f4dd068f7efe5817288
Clara Miño <claraa.mino12@gmail.com>
2025-06-30 14:01:40 -0300

    config para poder subir a github page

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Carrito-Simulador-React/',//nombre del repo
+  build: {
+    outDir: 'dist', // La carpeta de salida por defecto de Vite
+  }
 })
